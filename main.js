9deabbcbde3d08d5937e81f29dc8e7a026142703
CoderTvisha <71917050+CoderTvisha@users.noreply.github.com>
2022-06-01 17:50:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload(){
+    img = loadImage;
+
+}
+
+function setup(){
+    canvas = createCanvas(480,400);
+    canvas.position(110,250);
+    video = createCapture(VIDEO);
+   video.hide()
+   tint_color = "";
+}
+
+function draw(){
+    image(video, 0 , 0 , 480 , 400);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    save('filter_app.png');
+
+}
+
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
+}
